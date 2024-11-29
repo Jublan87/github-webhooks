5390152c2ceeab615f62878fdc1028680f7c9872
Blanco Caruso Juan <Juan.Blanco@externos.afpcapital.cl>
2024-11-29 14:46:00 -0300

    primer commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { envs } from './config';
+import { GithubController } from './presentation/github/controller';
+
+( () => {
+    main();
+})();
+
+function main() {
+    const app = express();
+
+    const controller = new GithubController();
+
+    app.post('/api/github', controller.webhookHandler);
+
+    app.listen(envs.PORT, () => {
+        console.log(`Server is running on port ${envs.PORT}`);
+    })
+}
